7f97dfb92d93a9c58e1f099e8d4fee5ca16ffd9f
JatziryBH <ziryz_10@hotmail.com>
2016-04-10 21:12:16 -0500

    Primer commit: Creación del proyecto y la estructura del mismo (MVC).

---
+++ ProyectoCalidad/src/Modelo/Conexion.java
@@ -0,0 +1,19 @@
+/*
+ * Techno Engineers
+ * Conexion
+ * 10/04/2016 -- Autorizo: Oscar de Paz Feliciano // Lider de proyecto
+ * Modificaciones: 
+ * Sirve para encapsular los datos de la tabla "usuarios" en la base de datos
+ * Numero de metodos en el codigo: 0
+ * Interfaces: 
+ */
+package Modelo;
+
+/**
+ *
+ * @author Techno Engineers
+ */
+public class Conexion
+{
+    
+}
