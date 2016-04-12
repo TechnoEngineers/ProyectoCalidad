f99c5ef5c17df5a42df664c1988822b78c6a6a14
oscardepaz <ing.en.software.oscardepaz@gmail.com>
2016-04-12 16:01:16 -0500

    Se crean las vistas de recuperar contrasena

--- ProyectoCalidad/src/Vista/Login.java
+++ ProyectoCalidad/src/Vista/Login.java
@@ -11,7 +11,11 @@ package Vista;
 
 import Modelo.UsuariosDatosEncapsulados;
 import Modelo.UsuariosModelo;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.font.TextAttribute;
 import java.sql.Connection;
+import java.util.Map;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -19,13 +23,20 @@ import javax.swing.JPanel;
  *
  * @author Techno Engineers
  */
-public class Login extends javax.swing.JFrame {
+public class Login extends javax.swing.JFrame
+{
 
     /**
      * Creates new form Login
      */
-    public Login() {
+    public Login()
+    {
         initComponents();
+        Font font = jLRecuperarContrasena.getFont();
+        Map attributes = font.getAttributes();
+        attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+        jLRecuperarContrasena.setFont(font.deriveFont(attributes));
+        jLRecuperarContrasena.setForeground(Color.blue);
     }
 
     @SuppressWarnings("unchecked")
@@ -34,17 +45,18 @@ public class Login extends javax.swing.JFrame {
     {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        jLNombre = new javax.swing.JLabel();
+        jLContrasena = new javax.swing.JLabel();
         jTFNombre = new javax.swing.JTextField();
         jPFContrasena = new javax.swing.JPasswordField();
         jBAcceder = new javax.swing.JButton();
+        jLRecuperarContrasena = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Nombre: ");
+        jLNombre.setText("Usuario: ");
 
-        jLabel2.setText("Contraseña: ");
+        jLContrasena.setText("Contraseña: ");
 
         jBAcceder.setText("Acceder");
         jBAcceder.setPreferredSize(new java.awt.Dimension(120, 50));
@@ -56,40 +68,48 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
+        jLRecuperarContrasena.setText("¿Olvideste contraseña?");
+        jLRecuperarContrasena.setToolTipText("");
+        jLRecuperarContrasena.addMouseListener(new java.awt.event.MouseAdapter()
+        {
+            public void mouseClicked(java.awt.event.MouseEvent evt)
+            {
+                jLRecuperarContrasenaMouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 133, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(97, 97, 97)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addGap(27, 27, 27)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jTFNombre)
-                            .addComponent(jPFContrasena, javax.swing.GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(142, 142, 142)
+                    .addComponent(jLContrasena)
+                    .addComponent(jLNombre)
+                    .addComponent(jPFContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTFNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jLRecuperarContrasena)
                         .addComponent(jBAcceder, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(63, Short.MAX_VALUE))
+                .addGap(117, 117, 117))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(80, 80, 80)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jTFNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(60, 60, 60)
+                .addComponent(jLNombre)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTFNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLContrasena)
+                .addGap(4, 4, 4)
+                .addComponent(jPFContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jPFContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(36, 36, 36)
                 .addComponent(jBAcceder, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(54, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLRecuperarContrasena)
+                .addContainerGap(41, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -115,52 +135,72 @@ public class Login extends javax.swing.JFrame {
     private void jBAccederActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jBAccederActionPerformed
     {//GEN-HEADEREND:event_jBAccederActionPerformed
         final JPanel jPanel = new JPanel();
-        
-        UsuariosDatosEncapsulados objUsuariosDatosEncapsulados=new UsuariosDatosEncapsulados();
+
+        UsuariosDatosEncapsulados objUsuariosDatosEncapsulados = new UsuariosDatosEncapsulados();
         objUsuariosDatosEncapsulados.setNombreUsuario(jTFNombre.getText());
         objUsuariosDatosEncapsulados.setContrasena(jPFContrasena.getText());
-        
-        int n = UsuariosModelo.consultarUsuarios(objUsuariosDatosEncapsulados);   
-        if (n == 0) {
+
+        int n = UsuariosModelo.consultarUsuarios(objUsuariosDatosEncapsulados);
+        if (n == 0)
+        {
             JOptionPane.showMessageDialog(jPanel, "Bienvenido", "Mensaje", JOptionPane.ERROR_MESSAGE);
 
-        } else {
+        } else
+        {
             JOptionPane.showMessageDialog(jPanel, "Acceso denegado.", "Mensaje", JOptionPane.ERROR_MESSAGE);
 
         }
-            
+
     }//GEN-LAST:event_jBAccederActionPerformed
+
+    private void jLRecuperarContrasenaMouseClicked(java.awt.event.MouseEvent evt)//GEN-FIRST:event_jLRecuperarContrasenaMouseClicked
+    {//GEN-HEADEREND:event_jLRecuperarContrasenaMouseClicked
+        VerificarUsuario objVerificarUsuario=new VerificarUsuario();
+        objVerificarUsuario.setVisible(true);
+       
+        
+    }//GEN-LAST:event_jLRecuperarContrasenaMouseClicked
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+        try
+        {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
+            {
+                if ("Nimbus".equals(info.getName()))
+                {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
+        } catch (ClassNotFoundException ex)
+        {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
+        } catch (InstantiationException ex)
+        {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
+        } catch (IllegalAccessException ex)
+        {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (javax.swing.UnsupportedLookAndFeelException ex)
+        {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
                 new Login().setVisible(true);
             }
         });
@@ -168,8 +208,9 @@ public class Login extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBAcceder;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLContrasena;
+    private javax.swing.JLabel jLNombre;
+    private javax.swing.JLabel jLRecuperarContrasena;
     private javax.swing.JPasswordField jPFContrasena;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField jTFNombre;
